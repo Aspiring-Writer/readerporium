a3b369d6d2cfe2a3caea08d1df84fe280c667ac9
Bravisha Skietano <yolson@liberty.edu>
2022-03-10 14:44:52 -0500

    fix sqlalchemy model error with heroku hack

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 login_manager = LoginManager()
